63eedaa3b840bdcc7312576955f3946fa72ef58f
montanari2019 <ikaro.montanari31@gmail.com>
2024-07-12 11:56:39 -0400

    depois de muito trabalho consegui me comunicar com o banco mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    MongooseModule.forRoot(process.env.DATABASE_URL, {
+      authSource: 'admin',
+    }),
+    // MongooseModule.forRoot("mongodb://credisulmongo:sicoob2024@localhost:27017/nestjs", {
+    //   authSource: "admin"
+    // })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
